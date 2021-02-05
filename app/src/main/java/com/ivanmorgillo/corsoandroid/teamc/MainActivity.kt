a6dfee58f46b55eefe0b82b74041cb73be793f06
abauz <abauzano@hotmail.it>
2021-02-05 13:01:57 +0100

    chore: added soem comments

--- app/src/main/java/com/ivanmorgillo/corsoandroid/teamc/MainActivity.kt
+++ app/src/main/java/com/ivanmorgillo/corsoandroid/teamc/MainActivity.kt
@@ -16,18 +16,21 @@ class MainActivity : AppCompatActivity() {
 
         // dobbiamo mettere l'adapter in comunicazione con la recyclerview
         recipes_list.adapter = adapter
-        //
+        // osserva in che stato si trova la schermata
         viewModel.states.observe(this, { state ->
             when (state) {
+                // Nasconde la progressBar e fa vedere la lista delle ricette
                 is MainScreenStates.Content -> {
                     recipes_list_progressBar.gone()
                     adapter.setRecipes(state.recipes)
                 }
+                // Mostra Errore
                 MainScreenStates.Error -> {
                     recipes_list_progressBar.gone()
                     Snackbar.make(recipes_list, getString(R.string.main_screen_error), Snackbar.LENGTH_LONG)
                         .show()
                 }
+                // ProgressBar visible
                 MainScreenStates.Loading -> {
                     recipes_list_progressBar.visible()
                 }
