693f1d442a4b8e5170623fedf9de79c3ec86c64a
GiulyBlues <giuliasansone98@gmail.com>
2021-02-09 12:51:36 +0100

    feat: added StrictMode

--- app/src/main/java/com/ivanmorgillo/corsoandroid/teamc/MyApplication.kt
+++ app/src/main/java/com/ivanmorgillo/corsoandroid/teamc/MyApplication.kt
@@ -1,6 +1,7 @@
 package com.ivanmorgillo.corsoandroid.teamc
 
 import android.app.Application
+import android.os.StrictMode
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -8,12 +9,30 @@ import org.koin.core.context.startKoin
 @Suppress("unused") // gestire falso positivo
 class MyApplication : Application() {
     override fun onCreate() {
+        setupScrictMode()
         super.onCreate()
         // Start Koin
         startKoin {
             androidLogger()
             androidContext(this@MyApplication)
             modules(appModule)
+        }
+    }
+
+    private fun setupScrictMode() {
+        if (BuildConfig.DEBUG) { // affinché venga usato solo della modalità debug
+            StrictMode.setThreadPolicy(
+                StrictMode.ThreadPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .build()
+            )
+            StrictMode.setVmPolicy(
+                StrictMode.VmPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog() // log in logcat
+                    .build()
+            )
         }
     }
 }
