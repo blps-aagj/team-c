d258e2309bb687822462d05e98a5770d73a75a87
JosePalomino <you@example.com>
2021-02-10 16:08:39 +0100

    feat : add menu refresh layout

--- app/src/main/java/com/ivanmorgillo/corsoandroid/teamc/MainActivity.kt
+++ app/src/main/java/com/ivanmorgillo/corsoandroid/teamc/MainActivity.kt
@@ -5,21 +5,18 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.no_network.*
+
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import android.view.Menu
-
-import android.view.MenuInflater
-
-
-
+import android.view.MenuItem
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     private val viewModel: MainViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)//eseguita 1 sola volta
+        setContentView(R.layout.activity_main) // eseguita 1 sola volta
 
         val adapter = RecipesAdapter {
             viewModel.send(MainScreenEvent.OnRecipeClick(it))
@@ -33,6 +30,8 @@ class MainActivity : AppCompatActivity() {
                 // Nasconde la progressBar e fa vedere la lista delle ricette
                 is MainScreenStates.Content -> {
                     recipes_list_progressBar.gone()
+                    main_screen_no_network.gone()
+                    recipes_list_root.visible()
                     adapter.setRecipes(state.recipes)
                 }
                 // Mostra Errore
@@ -45,7 +44,7 @@ class MainActivity : AppCompatActivity() {
                 MainScreenStates.Loading -> {
                     recipes_list_progressBar.visible()
                 }
-            }
+            }.exhaustive
         })
         viewModel.actions.observe(this, { action ->
             when (action) {
@@ -53,19 +52,28 @@ class MainActivity : AppCompatActivity() {
                     Toast.makeText(this, "Working in progress navigate to detail", Toast.LENGTH_SHORT).show()
                 }
                 MainScreenAction.ShowNoInternetMessage -> {
-                    recipes_list_progressBar.gone()
                     recipes_list_root.gone()
-                    setContentView(R.layout.no_network)
+                    main_screen_no_network.visible()
                     Toast.makeText(this, "Check your internet connection", Toast.LENGTH_SHORT).show()
                 }
-            }
+            }.exhaustive
         })
         viewModel.send(MainScreenEvent.OnReady)
+        // Timber.d("Wow")
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater = menuInflater
-        inflater.inflate(R.menu.refresh , menu)
+        inflater.inflate(R.menu.refresh, menu)
         return true
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.refresh_btn -> viewModel.send(MainScreenEvent.OnRefreshClick)
+            else -> Toast.makeText(this, "come se fosse Antani", Toast.LENGTH_SHORT).show()
+        }.exhaustive
+        return super.onOptionsItemSelected(item)
+    }
+
 }
--- app/src/main/java/com/ivanmorgillo/corsoandroid/teamc/MainViewModel.kt
+++ app/src/main/java/com/ivanmorgillo/corsoandroid/teamc/MainViewModel.kt
@@ -6,43 +6,61 @@ import androidx.lifecycle.viewModelScope
 import com.ivanmorgillo.corsoandroid.teamc.network.LoadRecipesError
 import com.ivanmorgillo.corsoandroid.teamc.network.LoadRecipesResult
 import kotlinx.coroutines.launch
+import timber.log.Timber
 
 class MainViewModel(val repository: RecipesRepository) : ViewModel() {
 
-    val states = MutableLiveData<MainScreenStates>()
+    val states = MutableLiveData<MainScreenStates>() // potremmo passarci direttamente loading // mai null
     val actions = SingleLiveEvent<MainScreenAction>()
     fun send(event: MainScreenEvent) {
         when (event) {
             // activity pronta
             MainScreenEvent.OnReady -> {
-                states.postValue(MainScreenStates.Loading) // visualizziamo progressbar mentre carica lista
-                viewModelScope.launch {
-                    when (val result = repository.loadRecipes()) {
-                        is LoadRecipesResult.Failure -> {
-                            when (result.error) {
-                                LoadRecipesError.NoInternet -> {
-                                    actions.postValue(MainScreenAction.ShowNoInternetMessage)
-                                }
-                                LoadRecipesError.NoRecipeFound -> TODO()
-                                LoadRecipesError.ServerError -> TODO()
-                                LoadRecipesError.SlowInternet -> TODO()
-                            }
-                        }
-                        is LoadRecipesResult.Success -> {
-                            val recipes = result.recipes.map {
-                                RecipeUI(
-                                    recipeName = it.name,
-                                    recipeImageUrl = it.image
-                                )
-                            }
-                            states.postValue(MainScreenStates.Content(recipes))
-                        }
-                    }
-                }
+                /*when (states.value ){
+                    // 4 scenari
+                    is MainScreenStates.Content -> TODO("NON DOVREMMO FARE NIENTE") // l'utente chiude e apre l'app
+                    MainScreenStates.Error -> TODO("FORSE SIAMO TORNATI NEL WIFI E ABBIAMO BISOGNO DELLA  LOADING")
+                    MainScreenStates.Loading -> TODO("L'UTENTE è USCITO DALL'APP MENTRE STAVA ANCORA IN LOADING(!?)")
+                    null -> TODO("da valutare lo stato iniziale della app")
+                }*/
+                // aggiungere exaustive
+                loadContent()
             }
             is MainScreenEvent.OnRecipeClick -> {
+                //add tracking
                 actions.postValue(MainScreenAction.NavigateToDetail(event.recipe))
             }
+            MainScreenEvent.OnRefreshClick -> {
+                // add tracking
+                loadContent()
+            }
+        }.exhaustive
+    }
+
+    private fun loadContent() {
+        states.postValue(MainScreenStates.Loading) // visualizziamo progressbar mentre carica lista
+        viewModelScope.launch {
+            when (val result = repository.loadRecipes()) {
+                is LoadRecipesResult.Failure -> {
+                    when (result.error) {
+                        LoadRecipesError.NoInternet -> {
+                            actions.postValue(MainScreenAction.ShowNoInternetMessage)
+                        }
+                        LoadRecipesError.NoRecipeFound -> TODO()
+                        LoadRecipesError.ServerError -> TODO()
+                        LoadRecipesError.SlowInternet -> TODO()
+                    }
+                }
+                is LoadRecipesResult.Success -> {
+                    val recipes = result.recipes.map {
+                        RecipeUI(
+                            recipeName = it.name,
+                            recipeImageUrl = it.image
+                        )
+                    }
+                    states.postValue(MainScreenStates.Content(recipes))
+                }
+            }.exhaustive
         }
     }
 }
@@ -56,6 +74,7 @@ sealed class MainScreenEvent {
     data class OnRecipeClick(val recipe: RecipeUI) : MainScreenEvent()
 
     object OnReady : MainScreenEvent()
+    object OnRefreshClick : MainScreenEvent()
 }
 
 // Stati che rappresentano la nostra schermata
