1175a7ac8d2442862e471d0ca21788979f92a5be
Andrea Bauzano <74606006+Abauzano9@users.noreply.github.com>
2021-02-11 15:57:05 +0100

    feat: creating fragment_detail.xml

--- app/src/main/java/com/ivanmorgillo/corsoandroid/teamc/home/HomeFragment.kt
+++ app/src/main/java/com/ivanmorgillo/corsoandroid/teamc/home/HomeFragment.kt
@@ -30,6 +30,7 @@ class HomeFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
+        setHasOptionsMenu(true)
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
