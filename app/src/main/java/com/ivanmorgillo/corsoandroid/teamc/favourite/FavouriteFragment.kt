f499cb0cc6358128dae8910088dfed4d2926cd6b
Aurelio Laudiero <laudieroaurelio@gmail.com>
2021-02-26 10:26:37 +0100

    feat: added useless method because of mi teammates

--- app/src/main/java/com/ivanmorgillo/corsoandroid/teamc/favourite/FavouriteFragment.kt
+++ app/src/main/java/com/ivanmorgillo/corsoandroid/teamc/favourite/FavouriteFragment.kt
@@ -1,6 +1,10 @@
 package com.ivanmorgillo.corsoandroid.teamc.favourite
 
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 
 class FavouriteFragment : Fragment() {
+    fun uselessMethod() {
+        Toast.makeText(context, "TO DO", Toast.LENGTH_SHORT).show()
+    }
 }
