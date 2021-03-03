1258ac68c1132c12b4bd7b2a635edfc803ba7e76
Andrea Bauzano <74606006+Abauzano9@users.noreply.github.com>
2021-03-03 09:54:54 +0100

    feat: added binding on HomeFragment and refactor

--- app/src/main/java/com/ivanmorgillo/corsoandroid/teamc/home/HomeFragment.kt
+++ app/src/main/java/com/ivanmorgillo/corsoandroid/teamc/home/HomeFragment.kt
@@ -1,70 +1,64 @@
 package com.ivanmorgillo.corsoandroid.teamc.home
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.ivanmorgillo.corsoandroid.teamc.MainScreenAction.NavigateToDetail
 import com.ivanmorgillo.corsoandroid.teamc.MainScreenEvent
+import com.ivanmorgillo.corsoandroid.teamc.MainScreenEvent.OnFavouriteClicked
+import com.ivanmorgillo.corsoandroid.teamc.MainScreenEvent.OnRecipeClick
+import com.ivanmorgillo.corsoandroid.teamc.MainScreenEvent.OnRefreshClick
 import com.ivanmorgillo.corsoandroid.teamc.MainScreenStates
+import com.ivanmorgillo.corsoandroid.teamc.MainScreenStates.Content
 import com.ivanmorgillo.corsoandroid.teamc.MainViewModel
 import com.ivanmorgillo.corsoandroid.teamc.R
+import com.ivanmorgillo.corsoandroid.teamc.databinding.FragmentHomeBinding
 import com.ivanmorgillo.corsoandroid.teamc.exhaustive
 import com.ivanmorgillo.corsoandroid.teamc.gone
+import com.ivanmorgillo.corsoandroid.teamc.home.HomeFragmentDirections.Companion.actionHomeFragmentToDetailFragment
+import com.ivanmorgillo.corsoandroid.teamc.utils.bindings.viewBinding
 import com.ivanmorgillo.corsoandroid.teamc.visible
-import kotlinx.android.synthetic.main.fragment_home.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import timber.log.Timber
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(R.layout.fragment_home) {
     private val viewModel: MainViewModel by viewModel()
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        setHasOptionsMenu(true)
-        return inflater.inflate(R.layout.fragment_home, container, false)
-    }
-
+    private val binding by viewBinding(FragmentHomeBinding::bind)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val adapter = RecipeByAreaAdapter(
-            { viewModel.send(MainScreenEvent.OnRecipeClick(it)) },
-            { viewModel.send((MainScreenEvent.OnFavouriteClicked(it))) }
+            { viewModel.send(OnRecipeClick(it)) },
+            { viewModel.send((OnFavouriteClicked(it))) }
         )
-        recipes_list.adapter = adapter
+        binding.recipesList.adapter = adapter
         viewModel.states.observe(viewLifecycleOwner, { state ->
             when (state) {
-                is MainScreenStates.Content -> {
-                    recipes_list_progressBar.gone()
-                    main_screen_no_network.gone()
-                    recipes_list_root.visible()
-                    adapter.setRecipesByArea(state.recipes)
-                }
+                is Content -> setupContent(adapter, state)
                 MainScreenStates.Loading -> {
-                    recipes_list_progressBar.visible()
+                    binding.recipesListProgressBar.visible()
                     Timber.d("MainscreenStates Loading")
                 }
                 MainScreenStates.Error.NoNetwork -> {
-                    recipes_list_progressBar.gone()
-                    recipes_list_root.gone()
-                    main_screen_no_network.visible()
+                    binding.recipesListProgressBar.gone()
+                    binding.recipesListRoot.gone()
+                    binding.mainScreenNoNetwork.root.visible()
                 }
                 MainScreenStates.Error.NoRecipeFound -> {
-                    recipes_list_progressBar.gone()
-                    recipes_list_root.gone()
-                    main_screen_no_recipe.visible()
+                    binding.recipesListProgressBar.gone()
+                    binding.recipesListRoot.gone()
+                    binding.mainScreenNoRecipe.root.visible()
                 }
             }.exhaustive
         })
         viewModel.actions.observe(viewLifecycleOwner, { action ->
             when (action) {
                 is NavigateToDetail -> {
-                    val directions = HomeFragmentDirections.actionHomeFragmentToDetailFragment(action.recipe.id)
+                    val directions = actionHomeFragmentToDetailFragment(action.recipe.id)
                     Timber.d("Invio al detail RecipeId = ${action.recipe.id}")
                     findNavController().navigate(directions)
                 }
@@ -74,13 +68,20 @@ class HomeFragment : Fragment() {
         Timber.d("Wow")
     }
 
+    private fun setupContent(adapter: RecipeByAreaAdapter, state: Content) {
+        binding.recipesListProgressBar.gone()
+        binding.mainScreenNoNetwork.root.gone()
+        binding.recipesListRoot.visible()
+        adapter.setRecipesByArea(state.recipes)
+    }
+
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
         inflater.inflate(R.menu.refresh, menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.refresh_btn) viewModel.send(MainScreenEvent.OnRefreshClick)
+        if (item.itemId == R.id.refresh_btn) viewModel.send(OnRefreshClick)
         return super.onOptionsItemSelected(item)
     }
 }
