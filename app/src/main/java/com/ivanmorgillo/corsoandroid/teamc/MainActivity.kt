ab51f83b6e2f2d146d91be181cc6be13ef132be7
ViscuintCode <jose.alcarrazpalo@gmail.com>
2021-03-15 11:20:44 +0100

    feat: added authentication on home screen

--- app/src/main/java/com/ivanmorgillo/corsoandroid/teamc/MainActivity.kt
+++ app/src/main/java/com/ivanmorgillo/corsoandroid/teamc/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.ivanmorgillo.corsoandroid.teamc
 
+import android.app.Activity
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.MenuItem
@@ -10,8 +12,14 @@ import androidx.browser.customtabs.CustomTabsIntent
 import androidx.core.view.GravityCompat
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 import com.ivanmorgillo.corsoandroid.teamc.databinding.ActivityMainBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import timber.log.Timber
+
+private const val RC_SIGN_IN: Int = 1234
 
 class MainActivity : AppCompatActivity() {
     private val viewModel: MainViewModel by viewModel()
@@ -63,13 +71,44 @@ class MainActivity : AppCompatActivity() {
                     true
                 }
                 R.id.about -> {
-                    Toast.makeText(this, "About da implementare", Toast.LENGTH_SHORT).show()
+                    // Choose authentication providers
+                    val providers = arrayListOf(
+                        AuthUI.IdpConfig.GoogleBuilder().build(),
+                    )
+                    startActivityForResult(
+                        AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setAvailableProviders(providers)
+                            .build(),
+                        RC_SIGN_IN
+                    )
+                    binding.drawerLayout.closeDrawer(GravityCompat.START)
                     true
                 }
                 else -> {
-                    Toast.makeText(this, "Antani", Toast.LENGTH_SHORT).show()
                     false
                 }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+                Timber.d("user : $user")
+                Toast.makeText(this, "Welcome, ${user.displayName}", Toast.LENGTH_LONG).show()
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+                Timber.e("autentication error : ${response?.error?.errorCode}")
             }
         }
     }
--- app/src/main/java/com/ivanmorgillo/corsoandroid/teamc/MyApplication.kt
+++ app/src/main/java/com/ivanmorgillo/corsoandroid/teamc/MyApplication.kt
@@ -2,6 +2,7 @@ package com.ivanmorgillo.corsoandroid.teamc
 
 import android.app.Application
 import android.os.StrictMode
+import androidx.viewbinding.BuildConfig
 import com.blps.aagj.cookbook.di.networkingKoinModule
 import com.ivanmorgillo.corsoandroid.teamc.firebase.CrashReportingTree
 import com.ivanmorgillo.corsoandroid.teamc.firebase.LineNumberDebugTree
