9e0c819206c8fca29891075275be79bcf35fd3e1
ViscuintCode <jose.alcarrazpalo@gmail.com>
2021-03-15 12:00:29 +0100

    feat: added signing on navigation drawer

--- app/src/main/java/com/ivanmorgillo/corsoandroid/teamc/MainActivity.kt
+++ app/src/main/java/com/ivanmorgillo/corsoandroid/teamc/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.ivanmorgillo.corsoandroid.teamc
 
 import android.app.Activity
-import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.browser.customtabs.CustomTabsIntent
@@ -18,8 +18,6 @@ import com.google.firebase.auth.FirebaseAuth
 import com.ivanmorgillo.corsoandroid.teamc.databinding.ActivityMainBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import timber.log.Timber
-
-private const val RC_SIGN_IN: Int = 1234
 
 class MainActivity : AppCompatActivity() {
     private val viewModel: MainViewModel by viewModel()
@@ -75,13 +73,11 @@ class MainActivity : AppCompatActivity() {
                     val providers = arrayListOf(
                         AuthUI.IdpConfig.GoogleBuilder().build(),
                     )
-                    startActivityForResult(
-                        AuthUI.getInstance()
-                            .createSignInIntentBuilder()
-                            .setAvailableProviders(providers)
-                            .build(),
-                        RC_SIGN_IN
-                    )
+                    val intent = AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build()
+                    firebaseAuthenticationResultLauncher.launch(intent)
                     binding.drawerLayout.closeDrawer(GravityCompat.START)
                     true
                 }
@@ -92,24 +88,21 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
+    private var firebaseAuthenticationResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
 
-        if (requestCode == RC_SIGN_IN) {
-            val response = IdpResponse.fromResultIntent(data)
+        val response = IdpResponse.fromResultIntent(result.data)
 
-            if (resultCode == Activity.RESULT_OK) {
-                // Successfully signed in
-                val user = FirebaseAuth.getInstance().currentUser
-                Timber.d("user : $user")
-                Toast.makeText(this, "Welcome, ${user.displayName}", Toast.LENGTH_LONG).show()
-            } else {
-                // Sign in failed. If response is null the user canceled the
-                // sign-in flow using the back button. Otherwise check
-                // response.getError().getErrorCode() and handle the error.
-                // ...
-                Timber.e("autentication error : ${response?.error?.errorCode}")
-            }
+        if (result.resultCode == Activity.RESULT_OK) {
+            // Successfully signed in
+            val user = FirebaseAuth.getInstance().currentUser
+            Timber.d("user ${user?.displayName}")
+            Toast.makeText(this, "Welcome, ${user?.displayName}", Toast.LENGTH_LONG).show()
+        } else {
+            // Sign in failed. If response is null the user canceled the
+            // sign-in flow using the back button. Otherwise check
+            // response.getError().getErrorCode() and handle the error.
+            // ...
+            Timber.e("authentication error  ${response?.error?.errorCode}")
         }
     }
 
