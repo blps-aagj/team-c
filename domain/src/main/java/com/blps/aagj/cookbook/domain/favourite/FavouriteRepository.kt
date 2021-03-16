7a07b7367b1f3dbdb2f29104917bdfc3266c9dac
Aurelio Laudiero <laudieroaurelio@gmail.com>
2021-03-16 09:44:35 +0100

    feat: added firestone database dependency, added coroutines-play-service 1.3.9 dependency. Implemented save favourite

--- domain/src/main/java/com/blps/aagj/cookbook/domain/favourite/FavouriteRepository.kt
+++ domain/src/main/java/com/blps/aagj/cookbook/domain/favourite/FavouriteRepository.kt
@@ -1,15 +1,7 @@
 import android.annotation.SuppressLint
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.ValueEventListener
-import com.google.firebase.database.ktx.database
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
-import com.google.gson.annotations.SerializedName
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import timber.log.Timber
-import kotlin.coroutines.resume
-import kotlin.coroutines.suspendCoroutine
+import kotlinx.coroutines.tasks.await
 
 interface FavouriteRepository {
     suspend fun loadAll(): List<Recipe>?
@@ -19,78 +11,37 @@ interface FavouriteRepository {
 }
 
 class FavouriteRepositoryImpl : FavouriteRepository {
-    private val db by lazy {
-        Firebase.database.reference
-    }
-    private val favourites = db.child("favourites")
 
-    override suspend fun loadAll(): List<Recipe>? = suspendCoroutine { continuation ->
-        favourites.get().addOnSuccessListener {
-            val entity = it.children
-                .mapNotNull {
-                    it.getValue(RecipeEntity::class.java)
-                }.mapNotNull {
-                    if (it.name != null && it.image != null && it.id != null) {
-                        Recipe(name = it.name, image = it.image, idMeal = it.id)
-                    } else {
-                        null
-                    }
-                }
-            continuation.resume(entity)
-        }
+    private val firestore by lazy {
+        Firebase.firestore
+    }
+
+    override suspend fun loadAll(): List<Recipe>? {
+        return emptyList()
     }
 
     @SuppressLint("ApplySharedPref")
-    override suspend fun save(recipe: Recipe, isFavourite: Boolean): Boolean = suspendCoroutine { continuation ->
-        if (isFavourite) {
-            val recipeEntity = RecipeEntity(name = recipe.name, image = recipe.image, id = recipe.idMeal)
-            favourites.child(recipe.idMeal.toString())
-                .setValue(recipeEntity)
-                .addOnFailureListener {
-                    Timber.e("Success save $it")
-                    continuation.resume(false)
-                }
-                .addOnSuccessListener {
-                    Timber.d("Successfully save recipe ${recipe.idMeal}")
-                    continuation.resume(true)
-                }
-        } else {
-            favourites.child(recipe.idMeal.toString())
-                .removeValue()
-                .addOnFailureListener {
-                    Timber.e("Failure remove $it")
-                    continuation.resume(false)
-                }
-                .addOnSuccessListener {
-                    Timber.d("Successfully remove recipe ${recipe.idMeal}")
-                    continuation.resume(false)
-                }
-        }
+    override suspend fun save(recipe: Recipe, isFavourite: Boolean): Boolean {
+        val favouriteMap = hashMapOf(
+            "id" to recipe.idMeal,
+            "name" to recipe.name,
+            "image" to recipe.image
+        )
+        firestore.collection("favourites").add(favouriteMap).await()
+        return true
     }
 
-    override suspend fun delete(id: Long) = withContext(Dispatchers.IO) {
-        favourites.child(id.toString()).removeValue()
-        true
+    override suspend fun delete(id: Long): Boolean {
+        return true
     }
 
-    override suspend fun isFavourite(id: Long): Boolean = suspendCoroutine {
-        favourites.child(id.toString()).addListenerForSingleValueEvent(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                val entity = snapshot.getValue(RecipeEntity::class.java)
-                val isFavourite = entity != null
-                it.resume(isFavourite)
-            }
-
-            override fun onCancelled(error: DatabaseError) = Unit
-        })
+    override suspend fun isFavourite(id: Long): Boolean {
+        return false
     }
 }
 
 data class RecipeEntity(
-    @SerializedName("name")
     val name: String? = null,
-    @SerializedName("image")
     val image: String? = null,
-    @SerializedName("id")
     val id: Long? = null
 )
