88c0fd661d3e38199b4fe611309b1df309c5e1ab
Andrea Bauzano <74606006+Abauzano9@users.noreply.github.com>
2021-03-19 12:50:18 +0100

    fix: changed androidx.fragment:fragment-ktx:1.2.0 to 1.3.0

--- app/src/main/java/com/ivanmorgillo/corsoandroid/teamc/search/SearchFragment.kt
+++ app/src/main/java/com/ivanmorgillo/corsoandroid/teamc/search/SearchFragment.kt
@@ -22,7 +22,6 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 import timber.log.Timber
 
 class SearchFragment : Fragment(R.layout.fragment_search) {
-    
     private val viewModel: RecipeSearchViewModel by viewModel()
     private val binding by viewBinding(FragmentSearchBinding::bind)
 
